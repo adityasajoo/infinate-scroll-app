136846bb25d0420a7f73638daa0b8e2e2b24970f
Adi <adityasajoo@gmail.com>
2023-04-18 01:03:06 +0530

    Add comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,44 @@ import fetchData from './utils/fetchData';
 import { useCallback, useMemo, useRef } from 'react';
 
 function App() {
+  /**
+   * Using the hook
+   */
   const { data, error, fetchNextPage, hasNextPage, isFetching, isLoading } =
     useInfiniteQuery('projects', fetchData, {
       getNextPageParam: (lastPage, pages) => lastPage.offset,
     });
 
+  /**
+   * Data is typically an array of pages,
+   * where each page contains an array of items.
+   * In order to easily map through and render these items,
+   * we can flatten the data into a single array.
+   */
   const flattenedData = useMemo(
     () => (data ? data?.pages.flatMap(item => item.results) : []),
     [data]
   );
 
+  /**
+   * Ref for our intersectionObserver that will be attached to the last element in the list
+   */
   const observer = useRef<IntersectionObserver>();
+
+  /**
+   * Logic to call fetchNextPage when the last element is visible on the screen
+   */
   const lastElementRef = useCallback(
     (node: HTMLDivElement) => {
-      if (isLoading) return;
+      //Return if already fetching
+      if (isLoading || isFetching) return;
+
+      // Disconnect if already observer exists
       if (observer.current) observer.current.disconnect();
+
+      //Create new observer for the last element, and call fetchNextPage if visible(isIntersecting)
       observer.current = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting && hasNextPage && !isFetching) {
+        if (entries[0].isIntersecting && hasNextPage) {
           fetchNextPage();
         }
       });
@@ -29,8 +50,14 @@ function App() {
     [isLoading, hasNextPage]
   );
 
+  /**
+   * Will be true only for the initial load
+   */
   if (isLoading) return <h1>Loading Data</h1>;
 
+  /**
+   * Show error if the API fails
+   */
   if (error) return <h1>Couldn't fetch data</h1>;
 
   return (
